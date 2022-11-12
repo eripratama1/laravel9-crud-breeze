4826dc94dfa6c51c1890b73c4e1deed58cf9fa25
Eri Pratama <eripratama90@gmail.com>
2022-11-12 10:10:55 +0800

    initial commit & setup breeze

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Author>
+ */
+class AuthorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $name = fake()->name();
+        $slug = Str::slug($name, '-');
+        
+        return [
+            'name' => $name,
+            'slug' => $slug
+        ];
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth','verified'])->group(function(){
+    Route::get('/dashboard',function(){
+        return view('dashboard');
+    })->name('dashboard');
+
+   Route::resource('books',(BookController::class));
+
+    
+});
+require __DIR__.'/auth.php';
